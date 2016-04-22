643fa0d8d0a47086004f05b147dfbad048d13c0b
jngaravitoc <nico.garavito@gmail.com>
2016-04-22 13:51:12 -0700

    contours code

--- project/contours.py
+++ project/contours.py
@@ -12,6 +12,11 @@ matplotlib.rc('font', **font)
 data_name = sys.argv[1]
 data = np.loadtxt(data_name)
 
+xi2 = data[:,3]
+mask = np.where((data[:,0]<85) & (data[:,0]>55) & (data[:,3]<30))[0]
+data_sorted = data[mask]
+print data_sorted
+
 N = math.ceil(len(data)**(1./3.))
 
 print 'The length of the matrix is going to be',N
@@ -20,22 +25,26 @@ print 'The length of the matrix is going to be',N
 min_xi2 = np.where(data[:,3] == min(data[:,3]))[0]
 # Finding the best Hubble constant value
 best_H_matrix = np.where(data[:,0] == data[min_xi2[0],0])[0]
+print "omega_m: ", data[min_xi2, 1][0]
+print "omega_L: ", data[min_xi2, 2][0]
+print "H0: ", data[min_xi2, 0][0]
+print "Max(L):", data[min_xi2,3][0]
 
+omega_m = np.linspace(min(data[best_H_matrix,1]), max(data[best_H_matrix,1]),N)
+omega_L = np.linspace(min(data[best_H_matrix,2]), max(data[best_H_matrix,2]),N)
 
-omega_m = np.linspace(min(data[best_H_matrix,1]), max(data[best_H_matrix,1]), N)
-omega_L = np.linspace(min(data[best_H_matrix,2]), max(data[best_H_matrix,2])    , N)
-
-print omega_m
-print min_xi2, best_H_matrix
+#print omega_m
+#print min_xi2, best_H_matrix
 # Making a 2x2 matrix to xi2 for the best H
 xis = data[best_H_matrix,3]
-xi2_matrix = np.reshape(xis,(int(N),int(N)))
+xi2_matrix = np.reshape(np.exp(-xis/2.),(int(N),int(N)))/np.max(np.max(-xis/2.))
 
 
-print xi2_matrix
-plt.contour(omega_m, omega_L, xi2_matrix)
+#print xi2_matrix
+#levels = [1-0.682, 1-.954, 1-.997, 1-.999]
+plt.contour(omega_m, omega_L, xi2_matrix)#, levels=levels)
 plt.colorbar()
 plt.xlabel('$\Omega_m$', fontsize=25)
 plt.ylabel('$\Omega_\Lambda$', fontsize=25)
-plt.savefig('test_contour.png', bbox_inches='tight', dpi=300)
-
+plt.show()
+#plt.savefig('test_contour.png', bbox_inches='tight', dpi=300)
